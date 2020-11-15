31fcb4d84bdee9d8a510b7e37e5e7230c7a41c41
Taromaru <minecraftmanlover100@gmail.com>
2020-11-15 04:21:00 +0000

    Adding music. Gn yall.

--- WhereTheMagicHappens.js
+++ WhereTheMagicHappens.js
@@ -5,7 +5,7 @@ const timer = setInterval(() => {
     readCount == count;
     
     document.getElementById("tweeturl").href = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fmurgn.github.io/sadfrogroll/&text=sadfrog%20was%20rolling%20for%20" + number_format(count, 2) + "%20seconds!&related=murgntheurgn&hashtags=sadfrogroll"
-    document.getElementById("counter").innerHTML = "He's been rolling for " + number_format(count, 2) + " seconds!";
+    document.getElementById("counter").innerHTML = number_format(count, 2)
 }, 10)
 
 function number_format(val, decimals){
@@ -13,4 +13,25 @@ function number_format(val, decimals){
     return val.toFixed(decimals);
 }
 
-document.getElementById("musicplayer").volume = 0.2;
+class music {
+    constructor(music) {
+        this.audioElement = new Audio();
+        this.audioElement.volume = 0.2;
+        this.music = music;
+        this.idx = -1;
+    }
+
+    GoToNext() {
+        this.idx++;
+
+        if (this.idx > this.music.length) {
+            this.idx = 0;
+        }
+        this.Play(this.idx)
+    }
+
+    Play(index) {
+        this.audioElement.src = music[this.idx]
+        this.audioElement.play();
+    }
+}
