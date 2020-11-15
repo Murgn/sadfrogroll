db03bd64b5269c58d1c8ca17565f7412b976afde
Murgn <68158297+Murgn@users.noreply.github.com>
2020-11-15 02:52:42 +0000

    look at my windows forms website pogu

--- docs/WhereTheMagicHappens.js
+++ docs/WhereTheMagicHappens.js
@@ -3,8 +3,9 @@ var readCount = 0;
 const timer = setInterval(() => {
     count += 0.01;
     readCount == count;
+    
     document.getElementById("tweeturl").href = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fmurgn.github.io/sadfrogroll/&text=sadfrog%20was%20rolling%20for%20" + number_format(count, 2) + "%20seconds!&related=murgntheurgn&hashtags=sadfrogroll"
-     document.getElementById("counter").innerHTML = "He's been rolling for " + number_format(count, 2) + " seconds!";
+    document.getElementById("counter").innerHTML = "He's been rolling for " + number_format(count, 2) + " seconds!";
 }, 10)
 
 function number_format(val, decimals){
@@ -12,3 +13,4 @@ function number_format(val, decimals){
     return val.toFixed(decimals);
 }
 
+document.getElementById("musicplayer").volume = 0.2;
