d1c1b8fc01c78c78124052d9409c2fe4fa28c636
Murgn <68158297+Murgn@users.noreply.github.com>
2020-11-15 01:38:03 +0000

    STUFF

--- docs/WhereTheMagicHappens.js
+++ docs/WhereTheMagicHappens.js
@@ -1,6 +1,9 @@
 var count = 0;
+var readCount = 0;
 const timer = setInterval(() => {
     count += 0.01;
+    readCount == count;
+    document.getElementById("tweeturl").href = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fmurgn.github.io/sadfrogroll/&text=sadfrog%20was%20rolling%20for%20" + number_format(count, 2) + "%20seconds!&related=murgntheurgn&hashtags=sadfrogroll"
      document.getElementById("counter").innerHTML = "He's been rolling for " + number_format(count, 2) + " seconds!";
 }, 10)
 
@@ -8,3 +11,4 @@ function number_format(val, decimals){
     val = parseFloat(val);
     return val.toFixed(decimals);
 }
+
